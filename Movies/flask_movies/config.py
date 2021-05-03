276cc0ef34f24dc447dd80385246da9b41098acd
earronlwalker <78825936+earronlwalker@users.noreply.github.com>
2021-05-02 23:38:07 -0400

    Delete Movies directory

--- Movies/flask_movies/config.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies_db.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SECRET_KEY = 'adsl7398lkasfjdl0938'
-    
--- Movies/main.py
+++
@@ -1,7 +0,0 @@
-from flask_movies import create_app
-
-app = create_app()
-
-
-if __name__=='__main__':
-    app.run(debug=True)
